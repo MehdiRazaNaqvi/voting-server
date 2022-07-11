e5bd6dc3a4d8d25dc63b70407e2df7d653e3a637
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-07-12 04:07:00 +0500

    initial

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express")
+var bodyParser = require('body-parser');
+
+const cors = require("cors")
+
+
+
+const app = express()
+app.use(cors())
+
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(bodyParser.json());
+
+
+
+
+let port = 4000
+
+app.listen(port, () => {
+
+
+
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+
+    const uri = "mongodb+srv://mehdi:mehdimongodb@cluster0.xuahs.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+    app.get("/getdata", (req, res) => {
+
+
+
+
+        client.connect(err => {
+
+            //   const collection = client.db("database0").collection("col0");
+
+
+            client.db("database0").collection("voting").find({}).toArray()
+                .then((ans) => res.send(ans))
+                .catch((err) => console.log(err))
+
+
+
+        })
+
+
+    })
+
+
+
+
+    app.post('/vote', (req, res) => {
+
+        const party = req.body.name
+
+        console.log(party)
+
+        client.connect(err => {
+
+            client.db("database0").collection("voting").updateOne({name : party },{$push:{ count : "firebasenew"  }})
+
+            .then((ans) => console.log(ans))
+            .catch((err) => console.log(err))
+
+
+
+
+        })
+
+
+
+
+    })
+
+
+
+
+
+
+})
